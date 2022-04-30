815a56b3d4b05a50af4650b8d1a0433c4428526c
nguyen huy <hyquaq@gmail.com>
2022-04-30 23:40:25 +0700

    change

--- N5T1/lab5/mergeSort/merge.c
+++ N5T1/lab5/mergeSort/merge.c
@@ -7,21 +7,12 @@ int opt_a;
 int opt_t;
 int opt_r;
 
-// number of elements in array
-//#define MAX 15
-//#define MAX 16
 int MAX;
 
-// number of threads
-//#define THREAD_MAX 4
 int THREAD_MAX;
 
-// using namespace std;
-
-// array of size MAX
 int *a;
 
-// thread control parameters
 struct tsk
 {
     int tsk_no;
@@ -29,11 +20,9 @@ struct tsk
     int tsk_high;
 };
 
-// merge function for merging two parts
 void merge(int low, int mid, int high)
 {
 
-    // n1 is size of left part and n2 is size of right part
     int n1 = mid - low + 1;
     int n2 = high - mid;
 
@@ -43,11 +32,9 @@ void merge(int low, int mid, int high)
     int i;
     int j;
 
-    // storing values in left part
     for (i = 0; i < n1; i++)
         left[i] = a[i + low];
 
-    // storing values in right part
     for (i = 0; i < n2; i++)
         right[i] = a[i + mid + 1];
 
@@ -55,7 +42,6 @@ void merge(int low, int mid, int high)
 
     i = j = 0;
 
-    // merge left and right in ascending order
     while (i < n1 && j < n2)
     {
         if (left[i] <= right[j])
@@ -64,11 +50,9 @@ void merge(int low, int mid, int high)
             a[k++] = right[j++];
     }
 
-    // insert remaining values from left
     while (i < n1)
         a[k++] = left[i++];
 
-    // insert remaining values from right
     while (j < n2)
         a[k++] = right[j++];
 
@@ -76,27 +60,21 @@ void merge(int low, int mid, int high)
     free(right);
 }
 
-// merge sort function
 void merge_sort(int low, int high)
 {
 
-    // calculating mid point of array
     int mid = low + (high - low) / 2;
 
     if (low < high)
     {
-        // calling first half
         merge_sort(low, mid);
 
-        // calling second half
         merge_sort(mid + 1, high);
 
-        // merging the two halves
         merge(low, mid, high);
     }
 }
 
-// thread function for multi-threading
 void *
 merge_sort123(void *arg)
 {
@@ -104,11 +82,9 @@ merge_sort123(void *arg)
     int low;
     int high;
 
-    // calculating low and high
     low = tsk->tsk_low;
     high = tsk->tsk_high;
 
-    // evaluating mid point
     int mid = low + (high - low) / 2;
 
     if (low < high)
@@ -121,7 +97,6 @@ merge_sort123(void *arg)
     return 0;
 }
 
-// Driver Code
 int main(int argc, char **argv)
 {
     clock_t start, end, duration;
@@ -133,13 +108,10 @@ int main(int argc, char **argv)
     MAX = 20;
     THREAD_MAX = 4;
 
-    // use new/general algorithm by default
     opt_a = 1;
 
-    // allocate the array
     a = malloc(sizeof(int) * MAX);
 
-    // generating random values in array
     if (opt_t)
         printf("ORIG:");
     for (int i = 0; i < MAX; i++)
@@ -189,24 +161,19 @@ int main(int argc, char **argv)
             printf("RANGE %d: %d %d\n", i, tsk->tsk_low, tsk->tsk_high);
     }
 
-    // creating 4 threads
     for (int i = 0; i < THREAD_MAX; i++)
     {
         tsk = &tsklist[i];
         pthread_create(&threads[i], NULL, merge_sort123, tsk);
     }
 
-    // joining all 4 threads
     for (int i = 0; i < THREAD_MAX; i++)
         pthread_join(threads[i], NULL);
-
-    // merging the final 4 parts
 
     merge(0, (MAX / 2 - 1) / 2, MAX / 2 - 1);
     merge(MAX / 2, MAX / 2 + (MAX - 1 - MAX / 2) / 2, MAX - 1);
     merge(0, (MAX - 1) / 2, MAX - 1);
 
-    // displaying sorted array
     printf("\n\nOutput:");
     for (int i = 0; i < MAX; i++)
         printf(" %d", a[i]);
